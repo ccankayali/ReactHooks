2c27b7ee9fc58e34007bba86d4c594008a699cea
Can Kayalı <volkand135@gmail.com>
2023-09-24 09:40:40 +0300

    Hooks

---
+++ hooks/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import React from 'react';
+import Us from './Hooks/us';
+import FibonacciUs from './Hooks/FibonacciUs';
+
+function App() {
+  return (
+    <div className="App">
+      <Us />
+      <br />
+      <FibonacciUs />
+    </div>
+  );
+}
+
+export default App;
